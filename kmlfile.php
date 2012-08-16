cb799f5f3c5b4ad07b06294c1d695c226fc401dc
Vladimir Kikhtenko <kva911@gmail.com>
2012-08-16 15:40:54 +0700

    fix

--- kmlfile.php
+++ kmlfile.php
@@ -21,9 +21,9 @@ if ($data === FALSE) {
     header("HTTP/1.0 404 Not Found");
     echo "<p>Not found file " . $file . "<p>";
 } else {
-    if (substr_compare($file, "kml", -3, 3)) {
+    if (substr_compare($file, "kml", -3, 3) === 0) {
         header("Content-Type: application/vnd.google-earth.kml+xml");
-    } else if (substr_compare($file, "png", -3, 3)) {
+    } else if (substr_compare($file, "png", -3, 3) === 0) {
         header("Content-Type: image/png");
     } else {
         header("Content-Type: application/octet-stream");
